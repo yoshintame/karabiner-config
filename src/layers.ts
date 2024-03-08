53761fbedac4b18646460f9f8cb3daa55e884a5f
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-03-08 14:08:01 +0300

    chore: add comments to layers

--- src/layers.ts
+++ src/layers.ts
@@ -1,5 +1,6 @@
 import { ifVar, map, toSetVar, withCondition } from "karabiner.ts";
 
+// Hyper condition on all letters layers is necessary because if it wasn't there, those buttons alone would be delayed due to the layer trigger
 export const layers = [
   map("⇪", "optionalAny")
     .toVar("hyper", true)
@@ -19,6 +20,9 @@ export const layers = [
     .toAfterKeyUp(toSetVar("f-layer", false))
     .toIfAlone("f")
     .condition(ifVar("hyper", true)),
+
+  // This condition is necessary, because without it hyper + (layer) + cmd + button want work with cmd behaviour.
+  // TODO: need to be invisigated why
   map("left_command", "optionalAny")
     .toVar("f-layer", true)
     .toAfterKeyUp(toSetVar("f-layer", false))
@@ -26,11 +30,23 @@ export const layers = [
     .condition(ifVar("hyper", true)),
 ];
 
-export const layerF = withCondition(
+// Explicit using of all existing layers in the conditions of necessity, then that without them the layers cease to be properly triggered
+// TODO: need to be invisigated why
+
+// unless is needed, because if use "ifVar("", false)" it want work
+// TODO: need to be invisigated why
+export const layerHyper = withCondition(
   ifVar("hyper", true),
   ifVar("s-layer", true).unless(),
   ifVar("d-layer", true).unless(),
-  ifVar("f-layer", true)
+  ifVar("f-layer", true).unless()
+);
+
+export const layerS = withCondition(
+  ifVar("hyper", true),
+  ifVar("s-layer", true),
+  ifVar("d-layer", true).unless(),
+  ifVar("f-layer", true).unless()
 );
 
 export const layerD = withCondition(
@@ -40,9 +56,9 @@ export const layerD = withCondition(
   ifVar("f-layer", true).unless()
 );
 
-export const layerHyper = withCondition(
+export const layerF = withCondition(
   ifVar("hyper", true),
   ifVar("s-layer", true).unless(),
   ifVar("d-layer", true).unless(),
-  ifVar("f-layer", true).unless()
+  ifVar("f-layer", true)
 );
