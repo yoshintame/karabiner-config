317f905307c84d653cafaf41d3a3e498d19804cd
Mikhail Ivanov <m.ivanov0427@gmail.com>
2025-04-09 08:10:54 +0700

    feat(keyboard-layout): add colemak-angle

--- src/keyboard-layouts.ts
+++ src/keyboard-layouts.ts
@@ -15,6 +15,15 @@ export const colemakLayoutRule = rule("Keyboard Layout").manipulators([
   withMapper({
     q: "q", w: "w", e: "f", r: "p", t: "b", y: "j", u: "l", i: "u", o: "y", p: ";",
     a: "a", s: "r", d: "s", f: "t", g: "g", h: "m", j: "n", k: "e", l: "i", ";": "o",
+    z: "z", x: "x", c: "c", v: "d", b: "v", n: "k", m: "h",
+  } as const)((from, to) =>
+    bindKeyboardLayout(from, to, [
+      ifVar("keyboard-layout", "colemak-angle"),
+      ifInputSource({ language: "en" }),
+    ])
+  ), withMapper({
+    q: "q", w: "w", e: "f", r: "p", t: "b", y: "j", u: "l", i: "u", o: "y", p: ";",
+    a: "a", s: "r", d: "s", f: "t", g: "g", h: "m", j: "n", k: "e", l: "i", ";": "o",
     z: "x", x: "c", c: "d", v: "v", b: "z", n: "k", m: "h",
   } as const)((from, to) =>
     bindKeyboardLayout(from, to, [
